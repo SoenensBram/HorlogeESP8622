1f659c625aa0a7bc9f5e2d940fe1a7e858bed922
Bram Soenens <bram@bram-rig.BramNet>
2020-12-09 09:44:19 +0100

    cleanup git repo

--- build/bootloader_support/src/flash_encrypt.d
+++
@@ -1,6 +0,0 @@
-src/flash_encrypt.o: \
- /home/bram/esp/ESP8266_RTOS_SDK/components/bootloader_support/src/flash_encrypt.c \
- /home/bram/Documents/vives/kortrijk/buisnesproject/Compilen/firmware/build/include/sdkconfig.h
-
-/home/bram/Documents/vives/kortrijk/buisnesproject/Compilen/firmware/build/include/sdkconfig.h:
-/home/bram/esp/ESP8266_RTOS_SDK/components/bootloader_support/src/flash_encrypt.c:
--- build/freemodbus/modbus/functions/mbfuncdiag.d
+++
@@ -1,3 +0,0 @@
-modbus/functions/mbfuncdiag.o: \
- /home/bram/esp/ESP8266_RTOS_SDK/components/freemodbus/modbus/functions/mbfuncdiag.c
-/home/bram/esp/ESP8266_RTOS_SDK/components/freemodbus/modbus/functions/mbfuncdiag.c:
